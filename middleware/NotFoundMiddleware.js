6ca0ca3f230381bfb53837737f0bc400100ea755
AeshahMaher3 <144242273+AeshahMaher3@users.noreply.github.com>
2024-12-08 00:47:33 +0300

    Create NotFoundMiddleware.js

---
+++ middleware/NotFoundMiddleware.js
@@ -0,0 +1,8 @@
+// notFoundMiddleware.js
+const notFoundMiddleware = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+module.exports = notFoundMiddleware;
