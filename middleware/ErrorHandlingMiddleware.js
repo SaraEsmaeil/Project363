d4e0e569c0e0147ba5e76e191ae07795efbd32fa
AeshahMaher3 <144242273+AeshahMaher3@users.noreply.github.com>
2024-12-08 01:06:20 +0300

    Delete middleware/ErrorHandlingMiddleware.js

--- middleware/ErrorHandlingMiddleware.js
+++
@@ -1,8 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-  console.error(err.stack); // Log the error (optional)
-  res.status(err.status || 500).json({
-    message: err.message || 'Internal Server Error',
-  });
-};
-
-module.exports = errorHandler;
