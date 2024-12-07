aa216befc8f12715a24400bbe3a4b34b96f086db
AeshahMaher3 <144242273+AeshahMaher3@users.noreply.github.com>
2024-12-08 01:03:21 +0300

    Delete middleware/NotFoundMiddleware.js

--- middleware/NotFoundMiddleware.js
+++
@@ -1,8 +0,0 @@
-// notFoundMiddleware.js
-const notFoundMiddleware = (req, res, next) => {
-  const error = new Error(`Not Found - ${req.originalUrl}`);
-  res.status(404);
-  next(error);
-};
-
-module.exports = notFoundMiddleware;
